c377f7d9ae8e48758192c00691d38dedec7ef4dc
bobr89 <toyer889@gmail.com>
2020-01-11 16:53:08 -0500

    Updates

---
+++ precache-manifest.4d16753f79ba716653531b9bffe8f448.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da81cece7469b31a450a23b18c28c34b",
+    "url": "/BrianC/index.html"
+  },
+  {
+    "revision": "1cacd4b841098b2b2b6b",
+    "url": "/BrianC/static/css/main.ad4e3930.chunk.css"
+  },
+  {
+    "revision": "ff0fd151240e0d2c678c",
+    "url": "/BrianC/static/js/2.70f2bfe0.chunk.js"
+  },
+  {
+    "revision": "2b91f9f2ad0264ae5f6ad18d6f76141d",
+    "url": "/BrianC/static/js/2.70f2bfe0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cacd4b841098b2b2b6b",
+    "url": "/BrianC/static/js/main.8ecc63e0.chunk.js"
+  },
+  {
+    "revision": "0add18a84355a0bb4edb",
+    "url": "/BrianC/static/js/runtime-main.a80f843c.js"
+  },
+  {
+    "revision": "61120cfdb4ca418e9cf2718a2a6fcda4",
+    "url": "/BrianC/static/media/cineplex.61120cfd.png"
+  },
+  {
+    "revision": "c2e2c43b699ee65f480402a584c46a0c",
+    "url": "/BrianC/static/media/harvard.c2e2c43b.png"
+  },
+  {
+    "revision": "d57a4874d6ba06ecc5f0325446dae2c5",
+    "url": "/BrianC/static/media/me.d57a4874.jpg"
+  },
+  {
+    "revision": "4f424af2d0f21432da52c2f4833ec2bb",
+    "url": "/BrianC/static/media/microsoft.4f424af2.jpg"
+  },
+  {
+    "revision": "55c5a170d73e5ed3d9612060f9160ea8",
+    "url": "/BrianC/static/media/resume.55c5a170.pdf"
+  },
+  {
+    "revision": "b9e42c0ee9a49418a3048c6951702237",
+    "url": "/BrianC/static/media/uw.b9e42c0e.jpg"
+  },
+  {
+    "revision": "1b6e8a8359ab5f42bd6a129f0edde934",
+    "url": "/BrianC/static/media/validusResearch.1b6e8a83.png"
+  },
+  {
+    "revision": "24fb9113b06f18b658855eccf9a0fefb",
+    "url": "/BrianC/static/media/via.24fb9113.png"
+  }
+]);
